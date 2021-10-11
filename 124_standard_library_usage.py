7e17e3bd9dd24922f6c9bbbf815d8d6135807c99
Ryan Meneses <meneses.ryan.j@gmail.com>
2021-10-11 09:42:35 -0700

    Initial commit

---
+++ 124_standard_library_usage.py
@@ -0,0 +1,15 @@
+# 124 Standard Library Usage (124_standard_library_usage)
+# US#80    Python Practice
+# Task#124 https://tree.taiga.io/project/hiyaryan-simoc-project-29/task/124?
+
+# Ryan Meneses
+# SER401 Capstone
+# Version 1.0
+
+# The Python Standard Library: https://docs.python.org/3/library/
+
+# Standard Libraries are distributed with Python
+# It is not necessary to import these libraries in order to use its functions
+
+# The following example demonstrates the usage of the Python math library
+
