a8dd97a7a50063066a2c0178672d19e9c79f3285
Ryan Meneses <meneses.ryan.j@gmail.com>
2021-10-11 17:31:17 -0700

    Add built-in len function example

--- 124_standard_library_usage.py
+++ 124_standard_library_usage.py
@@ -6,16 +6,29 @@
 # SER401 Capstone
 # Version 1.0
 
+# Standard libraries are distributed with Python and require the import keyword
+# View the link below for a list of libraries distributed with Python
 # The Python Standard Library: https://docs.python.org/3/library/
 
-# Standard Libraries are distributed with Python
+# There are many built-in functions available that do not require importing
+# View the link below for a list of built-in functions
+# Built-in Functions: https://docs.python.org/3/library/functions.html
+
+# I. MATH LIBRARY
 # The following example demonstrates how to use the ceil function in the math library
 
 # 1. Import the library
 import math
 
 x = 1.37
-
 # 2. Call the ceil function in the math library using the dot operator
-print(math.ceil(x))
+print("ceil(1.37) =", math.ceil(x))
+
+
+# II. BUILT-IN FUNCTIONS
+# The following example demonstrates how to use the len built-in function to determine the length of a string
+
+# 1. Call the len function directly
+string = "This is a string."
+print("len('This is a string.') =", len(string))
 
