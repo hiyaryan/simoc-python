e150406099e701a7506abec8f9c0bf1d1529443d
Ryan Meneses <meneses.ryan.j@gmail.com>
2021-10-13 15:53:50 -0700

    Initial commit

---
+++ 126_variables.py
@@ -0,0 +1,32 @@
+# 126 Variables (126_variables.py)
+# US#80    Python Practice
+# Task#126 https://tree.taiga.io/project/hiyaryan-simoc-project-29/task/126?
+
+# Ryan Meneses
+# SER401 Capstone
+# Version 1.0
+
+# This file demonstrates how to make and use variables in Python
+# Reference: https://realpython.com/python-variables/
+
+# Find more information on variables below
+# https://www.python.org/dev/peps/pep-0008/
+
+
+# CONTENTS
+# I. VARIABLE ASSIGNMENT
+
+# I. VARIABLE ASSIGNMENT
+# Variables do not need to be declared of defined in advance
+
+# Use the "=" to assign a variable to a value
+n = 300
+
+# To update a variable assign it to a new value
+n = 1000
+
+# Variables may be chained to the same value
+a = b = c = 300
+
+
+# II. VARIABLE TYPES
