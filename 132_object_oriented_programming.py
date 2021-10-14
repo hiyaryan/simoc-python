7fb67fa68ea770b9609885e1e16105552f1faaad
Ryan Meneses <meneses.ryan.j@gmail.com>
2021-10-14 11:07:37 -0700

    Add class definition

--- 132_object_oriented_programming.py
+++ 132_object_oriented_programming.py
@@ -17,3 +17,35 @@
 # I.   Class Definition
 # II.  Object Instantiation
 # III. Inheritance
+
+
+# I. CLASS DEFINITION
+# Class definitions start with the keyword "class"
+# Class names are written in PascalCase
+class Dog:
+    pass
+
+
+# Note: "pass" keyword is a placeholder allowing Python to compile minimally without error
+
+# Constructors are written using the method __init__()
+# The first parameter in __init__ is the keyword "self"
+#       New class instances are passed to "self" so that new "instance" attributes may be added
+# Attributes are written in __init__(); they are not declared globally and are called 'instance' attributes
+
+class Dog:
+    def __init__(self, name, age):
+        self.name = name # instance attribute
+        self.age = age   # instance attribute
+
+
+# Class attributes are attributes with the 'same' value for all class instances
+#       They must always be assigned an initial value
+class Dog:
+    species = "Canis familiaris"
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
