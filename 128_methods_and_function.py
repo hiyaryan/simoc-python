e3acdc3b92f653e583e739f77e8c0f3c2424cbae
Ryan Meneses <meneses.ryan.j@gmail.com>
2021-10-13 17:57:06 -0700

    Initial commit

--- 128_methods_and_function.py
+++ 128_methods_and_function.py
@@ -14,3 +14,5 @@
 
 
 # CONTENTS
+
+
---
+++ 132_object_oriented_programming.py
@@ -0,0 +1,18 @@
+# 132 Object-Oriented Programming (132_object_oriented_programming.py)
+# US#80    Python Practice
+# Task#132 https://tree.taiga.io/project/hiyaryan-simoc-project-29/task/132?
+
+# Ryan Meneses
+# SER401 Capstone
+# Version 1.0
+
+# This file demonstrates a simple object-oriented program.
+# Reference:
+
+# Find more information on object-oriented programming below.
+#
+
+
+# CONTENTS
+
+
