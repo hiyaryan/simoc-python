cacbd53551f230003797e14462e02b6c894be0dd
Ryan Meneses <meneses.ryan.j@gmail.com>
2021-10-13 17:00:49 -0700

    Initial commit

---
+++ 128_methods_and_functions.py
@@ -0,0 +1,16 @@
+# 128 Methods and Functions (128_methods_and_functions.py)
+# US#80    Python Practice
+# Task#128 https://tree.taiga.io/project/hiyaryan-simoc-project-29/task/128?
+
+# Ryan Meneses
+# SER401 Capstone
+# Version 1.0
+
+# This file demonstrates the difference between methods and functions
+# Reference:
+
+# Find more information on methods and functions below
+#
+
+
+# CONTENTS
