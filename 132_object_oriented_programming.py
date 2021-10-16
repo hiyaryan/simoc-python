d06a581a0dbd3db411d4aeea20375bfd7739d405
Ryan Meneses <meneses.ryan.j@gmail.com>
2021-10-16 06:02:33 -0700

    Add inheritance

--- 132_object_oriented_programming.py
+++ 132_object_oriented_programming.py
@@ -150,3 +150,112 @@ print(buddy)
 
 # Other dunder "double underscore: __" methods can be used to customize classes
 # More information here https://www.geeksforgeeks.org/customize-your-python-class-with-magic-or-dunder-methods/
+
+
+# III. INHERITANCE
+# "Inheritance is the process by which one class takes on the attributes and methods of another"
+# def. Children classes: newly formed classes
+# def. Parent classes: classes the children are derived from
+
+# 1. Children classes can override or extend attributes and methods of parent classes
+
+class Dog:
+    species = "Canis familiaris"
+
+    def __init__(self, name, age, breed):
+        self.name = name
+        self.age = age
+        self.breed = breed
+
+    # __str__ sets the string representation of the object
+    def __str__(self):
+        return f"{self.name} is {self.age} years old"
+
+    def speak(self, sound):
+        return f"{self.name} says {sound}"
+
+
+buddy = Dog("Buddy", 9, "Jack Russell Terrier")
+miles = Dog("Miles", 4, "Dachshund")
+jack = Dog("Jack", 3, "Bulldog")
+jim = Dog("Jim", 5, "Bulldog")
+
+print(buddy.speak("Yap"))
+print(jim.speak("Woof"))
+print(jack.speak("Woof"))
+
+
+# Create specific breed classes (children classes) to simplify working with the Dog class
+class Dog:
+    species = "Canis familiaris"
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # __str__ sets the string representation of the object
+    def __str__(self):
+        return f"{self.name} is {self.age} years old"
+
+    def speak(self, sound):
+        return f"{self.name} says {sound}"
+
+
+# Put the name of the parent class in the child class parenthesis to create inheritance
+class JackRusselTerrier(Dog):
+    pass
+
+
+class Dachshund(Dog):
+    pass
+
+
+class Bulldog(Dog):
+    pass
+
+
+buddy = JackRusselTerrier("Buddy", 9)
+miles = Dachshund("Miles", 4)
+jack = Bulldog("Jack", 3)
+jim = Bulldog("Jim", 5)
+
+print(miles.species)
+print(buddy.name)
+print(jack)
+print(jim.speak("Woof"))
+
+# Determine the class an object belongs to using the built-in function "type"
+print(type(miles))
+
+# Determine if a class is an instance of another class using the built-in function isinstance
+print(isinstance(miles, Dog))
+print(isinstance(miles, Bulldog))
+print(isinstance(jack, Dachshund))
+
+
+# 2. Extend the functionality of the Parent class
+class JackRusselTerrier(Dog):
+    # Extends functionality of parent class with default sound attribute "Arf"
+    def speak(self, sound="Arf"):
+        return f"{self.name} says {sound}"
+
+
+miles = JackRusselTerrier("Miles", 4)
+# Pass no arguments and return a predefined default value
+print(miles.speak())
+
+# Pass an argument and override the default value
+print(miles.speak("Grrr"))
+
+# Note that changes to methods in the parent class propagates to children classes if the methods are not overridden
+# To access the parent method that was overridden use the "super" keyword
+print(miles.speak())
+
+
+class JackRusselTerrier(Dog):
+    # Extends functionality of parent class with default sound attribute "Arf"
+    def speak(self, sound="Arf"):
+        return super().speak(sound)
+
+
+print(miles.speak())
