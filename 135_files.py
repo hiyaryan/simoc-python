6f37a7b0b32f2e4aa922f14df913d788328ddca2
Ryan Meneses <meneses.ryan.j@gmail.com>
2021-10-17 09:12:52 -0700

    Initial commit

---
+++ 135_files.py
@@ -0,0 +1,23 @@
+# 135 Files (132_files.py)
+# US#80    Python Practice
+# Task#135 https://tree.taiga.io/project/hiyaryan-simoc-project-29/task/132?
+
+# Ryan Meneses
+# SER401 Capstone
+# Version 1.0
+
+# This file demonstrates a simple object-oriented program.
+# Reference: Overview/.txt https://realpython.com/read-write-files-python/
+#            .csv Files    https://realpython.com/python-csv/
+#            .json Files   https://realpython.com/python-json/
+
+# Find more information on reading and writing files below.
+# Overview:   https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
+# .csv Files  https://docs.python.org/3/library/csv.html
+# .json Files https://docs.python.org/3/library/json.html
+
+
+# CONTENTS
+# I.   Reading and Writing Files (Overview and .txt Files)
+# II.  Reading and Writing .csv Files
+# III. Reading, Parsing, and Writinging .json Files
