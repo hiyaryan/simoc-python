fe65b14c5fac7de20e9473474bb4afe69d07ea38
Ryan Meneses <meneses.ryan.j@gmail.com>
2021-10-17 09:35:13 -0700

    Initial commit

---
+++ 140_object_oriented_concepts.py
@@ -0,0 +1,18 @@
+# 140 Object Oriented Concepts (140_object_oriented_concepts.py)
+# US#80    Python Practice
+# Task#140 https://tree.taiga.io/project/hiyaryan-simoc-project-29/task/140?
+
+# Ryan Meneses
+# SER401 Capstone
+# Version 1.0
+
+# This file demonstrates a simple object-oriented program.
+# Reference: Polymorphism  https://realpython.com/read-write-files-python/
+#            Abstraction   https://www.askpython.com/python/oops/abstraction-in-python
+#            Encapsulation https://www.askpython.com/python/oops/encapsulation-in-python
+
+# CONTENTS
+# I.   Polymorphism
+# II.  Abstraction
+# III. Encapsulation
+
