5a77160a853fc84e821b76ba3f40dfd95d95cfa4
merigrey <mgreytho@asu.edu>
2021-10-20 00:31:47 -0700

    US80 Task157 - Completed document by adding use with functions with parameters, chaining decorators, and general uses

--- 157_decorators.py
+++ 157_decorators.py
@@ -7,11 +7,17 @@
 # Version 1.0
 
 # This file demonstrates Python decorator functions
+# PLEASE NOTE - in general, this is an interesting Python feature which has some interesting uses. It's unlikely we will use too many of these
+# during our SIMOC implementation, but you never know!
 
 
 # CONTENTS
 #    I. Background
-#   II. General Syntax
+#   II. General Syntax and Use
+#  III. Decorating Functions with Parameters
+#   IV. Decorating a function multiple times
+#    V. General uses
+#   VI. Additional Resources
 
 
 # I. BACKGROUND
@@ -20,17 +26,17 @@
 # In order to do this, we use the function we want to extend as an argument in another function, which is wrapped in the decorator
 
 
-# II. GENERAL SYNTAX
+# II. GENERAL SYNTAX AND USE
 
 # Here is the 'normal' Python function
 def normal():
     print("This is the normal function.")
     
 # And here is a decorator:
-def decorator(func):    #takes in a function as an argument
+def example_decorator(func):    #takes in a function as an argument
     def inner():        
         print("I decorate the normal function!")
-        normal()
+        func()
         print("I have done my job.")
     return inner
 
@@ -41,16 +47,110 @@ def decorator(func):    #takes in a function as an argument
 
 # To use the decorator:
 
-decorated = decorator(normal)
-decorated()
+normal = example_decorator(normal)
+normal()
 
 # The above will print out:
 #    I decorate the normal function!
 #    This is the normal function.
 #    I have done my job.
 
+# Additional use - here is another 'normal' function:
 
-# ADDITIONAL RESOURCES
+@example_decorator
+def another_normal_function():
+    print("I'm the second normal function.")
+    
+# See that @decorator that? That is functionally equivalent to
+    # another_normal_function = decorator(another_normal_function)
+# So instead of calling
+    # another_normal_function = decorator(another_normal_function)
+    # another_normal_function()
+# You can just call this right off the bat:
+
+another_normal_function()
+
+# Which will print out:
+    # I decorate the normal function!
+    # I'm the second normal function
+    # I have done my job
+    
+    
+# III. DECORATING FUNCTIONS WITH PARAMETERS
+
+# In order to decorate functions that already take in parameters, we need to address this in the inner class.
+# NOTE: this inner function has access to the outer local functions.
+
+# Here is an example decorator and normal function:
+
+# this is the decorator
+def pretty_multiply(func):
+    def inner(a,b):
+        print("I'm going to multiply the provided integers:")
+        return func(a,b)
+    return inner
+
+# normal function
+@pretty_multiply
+def multiply(a,b):
+    print(a*b)
+    
+# calling the decorated function
+multiply(4,5)
+
+# Which will print:
+#    I'm going to multiply the provided integers:
+#    20
+
+    
+# IV. DECORATING A FUNCTION MULTIPLE TIMES
+# AKA. 'CHAINING'
+
+# You can decorate a function multiple times by 'chaining' decorators. For example:
+
+# Here is the first decorator
+def intro(func):
+    def inner(normal_text):
+        print("Now introducing... the normal function!\n")
+        func(normal_text)
+        print("\nThank you for joining us.")
+    return inner
+    
+# Here is the second decorator
+def stars(func):
+    def inner(normal_text):
+        print("*" * 27)
+        func(normal_text)
+        print("*" * 27)
+    return inner
+
+# And lastly, the normal function
+@intro
+@stars
+def chained_normal(normal_text):
+    print(normal_text)  
+
+chained_normal("I am the normal function!")
+
+# Which will print:
+#    Now introducing... the normal function!
+#
+#    ***************************
+#    I am the normal function!
+#    ***************************
+#
+#    Thank you for joining us. 
+
+
+# V. GENERAL USES
+# Decorators are commonly used for....
+#    Visuals (as above in the chaining example)
+#    Timing/Synchronization (e.g. delay function completion, making asynch functions look synchronized)
+#    Checking parameters, error messages, etc.
+#    For any function you use that you want to temporarily add functionality to
+
+
+# VI. ADDITIONAL RESOURCES
 # https://www.python.org/dev/peps/pep-0318/
 # https://www.geeksforgeeks.org/decorators-in-python/
 # https://www.programiz.com/python-programming/decorator
