2079680b1fad17fabba3130ddca252c22c3f3f19
merigrey <mgreytho@asu.edu>
2021-10-20 00:49:04 -0700

    US80_Task130 - Created document for use in this research. I thought I would work on this but I am actually tired now that it's almost 1am. zzzzzz.

---
+++ 130_methods.py
@@ -0,0 +1,28 @@
+# 130 Methods (130_methods.py)
+# US#80    Python Practice
+# Task#130 https://tree.taiga.io/project/hiyaryan-simoc-project-29/task/130
+
+# Meridith Greythorne
+# SER401 Capstone
+# Version 1.0
+
+# This file explains and demonstrates methods in Python in-depth
+
+
+# CONTENTS
+#    I. Background
+#   II. General Syntax and Use
+
+
+# I. BACKGROUND
+
+
+
+# II. GENERAL SYNTAX AND USE
+
+
+
+
+
+# ADDITIONAL RESOURCES
+# 
