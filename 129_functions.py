ebea96e30563f7ed3a68191b8e6f9ec0b0f9d77b
merigrey <mgreytho@asu.edu>
2021-10-19 22:37:05 -0700

    US80 Task 129 - finialized 129_functions.py per feedback, included best practices and Lambda information

--- 129_functions.py
+++ 129_functions.py
@@ -30,11 +30,13 @@ my_function()
 
 # II. BUILT-IN FUNCTIONS (reduced list)
 # Here is a list of built-in functions that might come in handy for our implementation
-# This is not a complete list - for all future work, please refer to the Python documentation here for all built-in functions: https://docs.python.org/3/library/functions.html
+# This is not a complete list - for all future work, please refer to the Python documentation here for all built-in functions: 
+# https://docs.python.org/3/library/functions.html
 # You can also see a full list of built-in functions in 128_methods_and_function.py
 #
 # abs(x) - Returns the absolute value of a number, x
-# bool([value]) - Converts the given value to true/false. Returns False for None, False, zero value in any type (0, 0.0, etc.), empty lists, empty tuples, empty strings, empty dictionaries.
+# bool([value]) - Converts the given value to true/false. Returns False for None, False, zero value in any type (0, 0.0, etc.), empty lists, 
+#     empty tuples, empty strings, empty dictionaries.
 # float([value]} - Returns a float number, taken from the number/string value
 # int([x]) - Returns an integer taken from the number/string x
 # len(o) - Returns a number per the length of object o, where o is a sequence or collection
@@ -61,19 +63,35 @@ def hello():
 # Call the function directly using parenthesis and passing any required arguments
 hello()
 
+# IV. BEST PRACTICES
+# When creating functions, keep it simple.
+# In order to keep the functions easy-to-understand...
+#    1. Keep the function small
+#    2. Keep it simple (do one thing)
+#    3. Use descriptive naming practices (e.g. not just 'my_func')
+#    4. Comments can be useful in describing the functionality, and are recommended if not required
+#    5. Minimize arguments required (many resources say that 4 is the upper end of # of arguments)
+#
+# Sometimes, the above can be hard. When running into an overly-complicated function, think about how it can be simplified or split up into 2 or 
+# more functions instead. It is easier to understand a function when it has a simple use.
 
-# IV. ANONYMOUS FUNCTIONS
-# These functions are not defined with keyword "def"
-# They do not have a body and are not directly called
+
+# IV. ANONYMOUS FUNCTIONS/LAMBDA FUNCTIONS
+# Summary
+#     Anonymous functions (AKA 'Lambda functions') are functions that are defined without a name, and are therefore not directly called. These functions...
+#         1. Are not defined with keyword "def", instead using the "lambda" keyword
+#         2. Do not have a body like a standard function
+
+#     The general syntax is as follows:
+#        lambda arguments: expression
 
 # Usage
-# Use lambda functions to shorten cde
+#     Use lambda functions to shorten code
 
 # Define an anonymous function with keyword lambda and 'n' number of arguments
 # lambda [arg1, arg2, ... argn]:expression
 
-# The following is an example of the same function defined using a standard function and
-# it's lambda equivalent
+# The following is an example of a standard function:
 
 # Normal function
 def linear_expression(x):
@@ -82,16 +100,27 @@ def linear_expression(x):
 
 print(linear_expression(2))
 
-# Lambda function with the same functionality
+
+# And here is aLambda function version with the same functionality as above:
 linear_expression_lambda = lambda x: (3 * x) + 2
 
 print(linear_expression_lambda(2))
+
+# Lambda functions are ESPECIALLY useful when you need a nameless, specific, simple function for a small amount of time. 
+
+# Commonly, Lambda functions are used as an argument for an existing function (e.g. built-in functions). For example, here is an example using the
+# built-in map() function. The map() function takes in a function and a list, and returns a map object of results from applying the function over the list.
+
+example_list = [8,5,3,8]
+
+result = map(lambda x: x+3, example_list)
+# This could also be cast to another list by encapsulating the right side in list(__)
 
 
 # V. CALLING A FUNCTION
 # Calling a function is executing a previously defined function
 def greet(name):
-    print(f'Hello {name}.')
+    print(f'Hello {name}.') #please note this is 'f-string' formatting. More info here: https://www.geeksforgeeks.org/formatted-string-literals-f-strings-python/
 
 
 # The next line is calling the defined function greet()
