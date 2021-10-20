ffbabd5d98774ce1fdbbc60e71f263adcd13bf95
merigrey <mgreytho@asu.edu>
2021-10-20 00:43:26 -0700

    US80 Task158 - created general document for use in researching and providing information regarding generators

---
+++ 158_generators.py
@@ -0,0 +1,33 @@
+# 158 Generators (158_generators.py)
+# US#80    Python Practice
+# Task#128 https://tree.taiga.io/project/hiyaryan-simoc-project-29/task/158
+
+# Meridith Greythorne
+# SER401 Capstone
+# Version 1.0
+
+# This file explains and demonstrates generators in Python
+
+
+# CONTENTS
+#    I. Background
+#   II. General Syntax and Use
+
+
+# I. BACKGROUND
+# IF A DEF BLOCK CONTAINS A 'yield', THE FUNCTION IS AUTOMATICALLY CONSIDERED A GENERATOR FUNCTION
+# This is because Generators generate values and uses 'yield' to do so, rather than 'return'. Generators make iterations easy, 
+# as you don't have to spend time implementing a specific iterator class, or keep track of states and timeout.
+# In it's primary use, a genorator will return an iterator object which we can iterate over easily.
+
+
+# II. GENERAL SYNTAX AND USE
+
+
+
+
+
+# ADDITIONAL RESOURCES
+# https://wiki.python.org/moin/Generators
+# https://www.programiz.com/python-programming/generator
+# https://www.geeksforgeeks.org/generators-in-python/
