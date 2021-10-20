3e6c5fbf72055537ef05b77955d5cd5456cce4bc
Ryan Meneses <meneses.ryan.j@gmail.com>
2021-10-20 10:03:59 -0700

    Initial commit

---
+++ 131_scripts.py
@@ -0,0 +1,17 @@
+# 131 Scripts (131_scripts.py)
+# US#80    Python Practice
+# Task#131 https://tree.taiga.io/project/hiyaryan-simoc-project-29/task/131?
+
+# Ryan Meneses
+# SER401 Capstone
+# Version 1.0
+
+# This file demonstrates a simple object-oriented program.
+# Reference: https://realpython.com/python3-object-oriented-programming/
+
+# Find more information on object-oriented programming below.
+# https://docs.python.org/3/tutorial/classes.html
+
+
+# CONTENTS
+# I.
