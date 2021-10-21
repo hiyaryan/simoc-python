d10d5b4db5ad5fb904d16f2091e41a58ee4b23e7
GregRRoss <grross1@asu.edu>
2021-10-20 22:31:33 -0700

    Add Interpolator/Server Ezio Recommended changes Total Changes
    Change not made: Asyncio not implemented... I still need to learn that.
    
    CLIENT SERVER CHANGES
    
    1. While(1) - > while True
    2. Change indents to 4 spaces.
    3. Change bytes(keyInput, encoding='utf8') to keyInput.encode('utf-8')
    4. keyInput.lower() == quit instead of quit || Quit
    5. Clearer output when client quits on server
    6. sendall message to b' %s format from f-string
    7. Make parenthesis consistent.
    8. Eliminate excess white space at the end of lines.
    9. Moved client print redundant code in server to print_clients function
    
    Interpolator CHANGES
    
    1. Remove \ in parenthesis since it is unneeded
    2. Remove ; from end of lines
    3. Add spaces between single operators and operands.
    4. Remove space between operand and operators in multi-step operation.
    5. Simplify math operation to one line instead of two for generation step.
    6. Simplify generate_fake_pair to return list without instatiating it.
    7. Convert tuples of {"name":value} to dict(name=value).
    8. Eliminate extraneous list creation and duplication for functions returning list.
    9. Simplify code with multiple assignments on single line.
    10. Simplify **kwargs in fake sensor to just take key words directly.
    11. I am still passing in a list to the fake sensor because the other thread needs to see the data appended to this list in real time. If I had created a list here and returned it, then the other thread could not interpolate the data as it was gathered in from the sensor. In theory this sensor could gather data in an infinite loop forever and the program would function forever (if the other thread was also set to be in a forever loop).
    12. Changed duplication of list from table_pair = first_pair[:] to table_pair = [first_pair]
    13. Eliminate table_pair by passing values directly.
    14. args passed to thread can be args=args to make clearer.
    15. Eliminate space before the : in functions, if, while, etc.
    16. Move multi-line comments from end of code line to before the code line.
    17. In my python for `index, current_pair in enumerate(data):` statement, current pair becomes (seconds, value)
    18. Packed my interpolate args by unpacking lists that made it up instead of individually addressing list indexes
    19. Interpolate args packed directly into function call instead of intermediate step.
    20. Put column names before with statements to unduplicate code.
    21. Change indents to 4 spaces.
    22. Removed trailing spaces from each line of code.

--- 139_Client_Server/python_client.py
+++ 139_Client_Server/python_client.py
@@ -1,27 +1,29 @@
 import socket
 import time
-def client():
-  client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  server_ip = 'localhost'
-  server_port = 8000 
-  server_address = (server_ip, server_port)
-  client_socket.connect(server_address)
-  print("Connection succesful!")
-  try:
-    while(1):
-      print("Type 'quit' to quit.")
-      data = client_socket.recv(80)
-      print(f"Server says: {data.decode('utf-8')}")
-      keyInput = input("Response: ")
 
-      client_socket.sendall(bytes(keyInput, encoding='utf8'))
-      if ((keyInput=='quit') or (keyInput=='Quit')):
-        time.sleep(5)
-        break
-      else: 
-         data = client_socket.recv(80)
-         print(f"Server says: {data.decode('utf-8')}")
-  finally:
+def client():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_ip = 'localhost'
+    server_port = 8000
+    server_address = (server_ip, server_port)
+    client_socket.connect(server_address)
+    print("Connection succesful!")
+    try:
+        while True:
+            print("Type 'quit' to quit.")
+            data = client_socket.recv(80)
+            print(f"Server says: {data.decode('utf-8')}")
+            keyInput = input("Response: ")
+
+            client_socket.sendall(keyInput.encode('utf-8'))
+            if keyInput.lower() == 'quit':
+                time.sleep(5)
+                break
+            else:
+                data = client_socket.recv(80)
+                print(f"Server says: {data.decode('utf-8')}")
+    finally:
       client_socket.close()
 
+#Start the client!
 client()
--- 139_Client_Server/python_server.py
+++ 139_Client_Server/python_server.py
@@ -1,63 +1,59 @@
 import socket # Gives us #socket.socket() and socket.sock_stream()
 import threading # For multiple connections
 
-clients = []
-
-
 # Thread for each client
-def serve(client_connect, client_address, client_id):
-  global clients
-  try:
-    print(f"Serve {client_id}")
-    while(1):
-      client_connect.sendall(bytes('Write something!',encoding='utf8'))
-      data = client_connect.recv(80)
-      data = data.decode('utf-8')
-      print(f"Got from {client_id} : {data}")
-      if ((data=='quit') or (data=='Quit')):
-        break
-      else:
-        client_connect.sendall(bytes(f'You sent {str(data)}',encoding='utf8'))
-  finally:
-    #Close Connection
-    client_connect.close()
-    clients.remove(client_id)
-    print(clients)
-    print(f"Number of current Connections: {len(clients)}")
-  
+def serve(client_connect, client_address, client_id, clients):
+    try:
+        print(f"Serve {client_id}")
+        while True:
+            client_connect.sendall(bytes('Write something!',encoding='utf8'))
+            data = client_connect.recv(80)
+            data = data.decode('utf-8')
+            print(f"Got from {client_id} : {data}")
+            if data.lower() == 'quit':
+                print(f"{client_id} has left!")
+                break
+            else:
+                client_connect.sendall(b'You sent %s' % data.encode('utf8'))
+    finally:
+        #Close Connection
+        client_connect.close()
+        clients.remove(client_id)
+        print_clients(clients)
+
+#Function to print current clients
+def print_clients(clients):
+        print("Number of current Connections: ", len(clients))
+        print("List of clients: ", clients)
+
 #Function to establish a TCP host
 def server():
-  global clients
-  client_id = 0
-  
-  while(1):
-    print(f"Number of current Connections: {len(clients)}")
-    print(clients)
-    # I have no idea what these parameters do: AF_INET, SOCK_STREAM
-    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_ip, server_port = 'localhost', 8000
-    host_address = (server_ip, server_port) #param1 = IP, param2 = port
-    #The bind() function assigns the socket to the address
-    tcp_socket.bind( host_address )
-    # The socket must then be set to listen mode
-    tcp_socket.listen(1)
-    print(f"Waiting for connection on {server_ip} port {server_port}")
-    #Get client
-    client_connect, client_address = tcp_socket.accept()
-    client_id += 1
-    client_details = client_connect, client_address, client_id
+    clients = []
+    client_id = 0
 
-    print(f"Client connected from {client_address}!")
-    #Start a thread for a sensor sending data to the system
-    server_thread = threading.Thread(target=serve, args=(client_details));
-    
-    clients.append(client_id)
-    server_thread.start()
+    while True:
+        print_clients(clients)
+        # I have no idea what these parameters do: AF_INET, SOCK_STREAM
+        tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_ip, server_port = 'localhost', 8000
+        host_address = (server_ip, server_port) #param1 = IP, param2 = port
+        #The bind() function assigns the socket to the address
+        tcp_socket.bind(host_address)
+        # The socket must then be set to listen mode
+        tcp_socket.listen(1)
+        print(f"Waiting for connection on {server_ip} port {server_port}")
+        #Get client
+        client_connect, client_address = tcp_socket.accept()
+        client_id += 1
+        client_details = client_connect, client_address, client_id, clients
 
+        print(f"Client connected from {client_address}!")
+        #Start a thread for a sensor sending data to the system
+        server_thread = threading.Thread(target=serve, args=(client_details))
 
-  # End Server Function
+        clients.append(client_id)
+        server_thread.start()
+    # End Server Function
 
-
-
+#Start the server!
 server()
-
