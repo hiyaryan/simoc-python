59eb42c237d3a0f8945223e2a016210215288e4a
merigrey <mgreytho@asu.edu>
2021-10-23 11:18:12 -0700

    US80 Task 127 - created doc and populated resources, moving onto elaboration

---
+++ 127_strongly_typed_and_errors.py
@@ -0,0 +1,26 @@
+# 127 Strongly Typed and Common Errors (127_strongly_typed_and_errors.py)
+# US#127    Python Practice
+# Task#127 https://tree.taiga.io/project/hiyaryan-simoc-project-29/task/127
+
+# Meridith Greythorne
+# SER401 Capstone
+# Version 1.0
+
+# This file demonstrates Python as a strongly-typed language, and shows some of the standard errors we might encounter
+
+
+# CONTENTS
+#    I. What is a strongly typed language?
+
+#    XX. Resources
+
+
+
+
+
+
+
+# RESOURCES
+# https://www.futurelearn.com/info/courses/python-in-hpc/0/steps/65121
+# https://towardsdatascience.com/dynamic-typing-in-python-307f7c22b24e
+# https://pythonconquerstheuniverse.wordpress.com/2009/10/03/static-vs-dynamic-typing-of-programming-languages/
