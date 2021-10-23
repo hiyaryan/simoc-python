d0c8459eb683fbe0ad738cb4152207d5a3a3ff28
Ryan Meneses <meneses.ryan.j@gmail.com>
2021-10-23 12:13:43 -0700

    Initial commit

---
+++ 189_asyncio.py
@@ -0,0 +1,24 @@
+# 189 Asynchronous I/O (189_asyncio.py)
+# US#80    Python Practice
+# Task#189 https://tree.taiga.io/project/hiyaryan-simoc-project-29/task/189?
+
+# Ryan Meneses
+# SER401 Capstone
+# Version 1.0
+
+# This file demonstrates how to write concurrent code using the asyncio library.
+# References: https://realpython.com/async-io-python/
+
+# Find official python documentation below
+# https://docs.python.org/3/library/asyncio.html
+
+# CONTENTS
+# I.   Overview
+# II.  asyncio Package and async/await
+# III. Async IO Design Patterns
+# IV.  Async IO's Roots in Generators
+# V.   Full Program Example
+# VI.  Async IO in Context
+# VII. Other asyncio Functions
+
+# I. Overview
