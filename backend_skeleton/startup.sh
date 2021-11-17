ab3e7e8a22dad7bb8ea84ccb072818ffa1f3f00c
GregRRoss <grross1@asu.edu>
2021-11-16 19:10:33 -0800

    Retain keyboard_sensor as additional sensor

--- backend_skeleton/startup.sh
+++ backend_skeleton/startup.sh
@@ -2,5 +2,6 @@ x-terminal-emulator -e python3 central.py
 sleep 2
 x-terminal-emulator -e python3 front.py
 sleep 2
+x-terminal-emulator -e python3 keyboard_sensor.py
 export BLINKA_MCP2221=1
 python3 sensor.py
