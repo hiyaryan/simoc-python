99e8cef3276af4abe31d19f57519c1ce9e4301a6
GregRRoss <grross1@asu.edu>
2021-11-29 20:25:35 -0800

    add change to abm (broken)

--- backend_skeleton/commonio.py
+++ backend_skeleton/commonio.py
@@ -12,7 +12,7 @@ def check_for_quit(keyInput):
 
 def set_socket():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_ip = 'localhost'
+    server_ip = '172.18.0.4'
     server_port = 17400
     server_address = (server_ip, server_port)
     client_socket.connect(server_address)
