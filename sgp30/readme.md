07ed24c1232d853b2ef0ae5ed3b3ba26cd8e0f33
GregRRoss <grross1@asu.edu>
2022-03-01 12:34:55 -0800

    Provide ppm instead of ticks for H2 & Ethanol

--- sgp30/readme.md
+++ sgp30/readme.md
@@ -1,9 +1,49 @@
 To install the SGP30:
 
-First be sure that you have enabled I2C on the Raspberry Pi. See the readme.md for the bme288 for more information.
+First be sure that you have enabled I2C on the Raspberry Pi. See the readme.md
+for the bme288 for more information.
 
 To install the adafruit library:
 `pip3 install adafruit-circuitpython-sgp30`
 
-This sensor reports Hydrogen gas and Ethanol readings in "Raw Ticks" that seem to be inversly proportional to the gas content a centimeter above whiskey.
-It also reports eCO2 and TVOC in ppm and ppb respectively, an estimate of CO2 from the other gases, and total volatile organic compounds.
+This sensor reports Hydrogen gas and Ethanol readings in "Raw Ticks" that 
+seem to be inversly proportional to the gas content a centimeter above whiskey.
+It also reports eCO2 and TVOC in ppm and ppb respectively, an estimate of CO2
+from the other gases, and total volatile organic compounds.
+
+According to documentation found for the sensor, Raw ticks can be converted into 
+concentrations by the following equation:
+
+concentration = concentration_reference*e^((signal_reference-signal_output)/512)
+where signal_reference is the signal given by the sensors at 0.5 ppm Hydrogen Gas.
+signal_output is for raw ticks, and the concentration_reference
+is 0.5 ppm for Hydrogen gas and 0.4 ppm for Ethanol.  Unfortunately, there is no
+known source for signal_reference that I can find without calibrating against
+a known concentration. It can maybe be inferred by making eCO2 match the output
+of SCD-30 CO2 somehow.
+
+0.3 to 30 ppm is typical ethanol concentration
+0.5 to 3 ppm is typical H2 concentration
+
+The range of the sensor is 0 to 1000 ppm.
+I get a 5 digit readings like 13827 with a change in the ones place
+So if variation is 1/10,000 there must be enough bits to hold at least 10,000. (14)
+I speculate 16 bits are used (65,536 steps?)
+
+For Hydrogen where I am getting 13827 now
+Based on this speculation ~0.5-3 = 0.5*e^(( sref - 13827) / 512)
+Assume h2 ppm is in the middle or ~ 1.25
+Solve for sref 1.25 = 0.5*e^(( sref - 13827) / 512)
+sref would be ~ 14296.1 based on this assumed concentration of 1.25 ppm
+
+For Ethanol where I am getting 19146 now
+Based on this speculation ~0.3-30 = 0.4*e^(( sref - 19146) / 512)
+Assume ethanol ppm is in the middle or ~ 14.85
+Solve for sref 14.85 = 0.4*e^(( sref - 19146) / 512)
+sref would be ~ 20996.5 based on this assumed concentration of 14.85 ppm
+
+
+Until further calibration I propose:
+
+concentration = 0.5*e^((14296-signal_output)/512)  for h2
+concentration = 0.4*e^((20997-signal_output)/512)  for ethanol
--- sgp30/sgp30_basic_run.py
+++ sgp30/sgp30_basic_run.py
@@ -13,23 +13,41 @@ def sensor_init():
 	sensor.set_iaq_baseline(0x8973, 0x8AEE) # Numbers from adafruit example
 	# sensor.set_iaq_humidity(self, gramsPM3) Humidity can be set
 	return sensor
-	
+
+
+def tick_Conversion_Ethanol(signal_output):
+    """ Gets raw ticks and converts to ppm """
+# Assumes calibration ethanol was 14.85 ppm which is typical but not measured
+    return 0.4*2.71828**((20997-signal_output)/512)
+
+def tick_Conversion_H2(signal_output):
+    """ Gets raw ticks and converts to ppm """
+# Assumes calibration H2 was 1.25 ppm which is typical but not measured
+    return 0.5*2.71828**((14296-signal_output)/512)
+
+
 def sensor_loop(sensor):
-	print(f"SGP30 serial numbers {sensor.serial} is in use") # Convert serial to hex for support purposes
-	# Baseline can be set with sensor.set_iaq_baseline(c02eq_base, tvoc_base)
-	print(f"c02eq baseline {sensor.baseline_eCO2} ")
-	print(f"TVOC baseline {sensor.baseline_TVOC} ")
-	count = 0;
-	while True:
-		if count > 20: #First 10-20 readings are not valid while sensor warms up
-			print("------------------------")
-			print(f"eCO2: {sensor.eCO2} ppm") # While warming up, always reports 400. ECO2 skyrockets above whiskey
-			print(f"TVOC: {sensor.TVOC} ppb") # While warming up, always reports 0. TVOC skyrockets above whiskey
-			print(f"H2: {sensor.H2} Raw Ticks") # Not sure on units. Drops from 12000 to 9000 a few cm above whiskey
-			print(f"Ethanol: {sensor.Ethanol} Raw Ticks") # This number seems to be 17000 away from whiskey and 14000 above the surface. 
-			print("------------------------")
-		time.sleep(1)
-		count += 1;		
+    print(f"SGP30 serial numbers {sensor.serial} is in use") # Convert serial to hex for support purposes
+    # Baseline can be set with sensor.set_iaq_baseline(c02eq_base, tvoc_base)
+    print(f"c02eq baseline {sensor.baseline_eCO2} ")
+    print(f"TVOC baseline {sensor.baseline_TVOC} ")
+    count = 0;
+    while True:
+        if count > 20: #First 10-20 readings are not valid while sensor warms up
+            H2_raw = sensor.H2
+            Ethanol_raw = sensor.Ethanol
+            H2 = tick_Conversion_H2 (H2_raw)
+            Ethanol = tick_Conversion_Ethanol ( Ethanol_raw )
+            print("------------------------")
+            print(f"eCO2: {sensor.eCO2} ppm") # While warming up, always reports 400. ECO2 skyrockets above whiskey
+            print(f"TVOC: {sensor.TVOC} ppb") # While warming up, always reports 0. TVOC skyrockets above whiskey
+            print(f"H2: {H2_raw} Raw Ticks") #  Drops from 12000 to 9000 a few cm above whiskey
+            print(f"Ethanol: {Ethanol_raw} Raw Ticks") # This number seems to be 17000 away from whiskey and 14000 above the surface. 
+            print(f"H2: {H2} ppm")  # Shoots up from around 1.25 to 350 ppm over whisky!
+            print(f"Ethanol: {Ethanol} ppm")  # shoots up from 14.85 ppm to 2250 ppm over whisky!
+            print("------------------------")
+        time.sleep(1)
+        count += 1;
 	
 
 sensor = sensor_init()
